4ee25ad9095a9b0c5648b09e806b00b4b5354066
Romina Martin <rominamartin1d@gmail.com>
2018-08-24 19:35:46 +0200

    Month displayed is the current one

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
 import "../styles/index.scss";
+/*** CONSTANTS ***/
 const CALENDAR = document.getElementById("calendar");
+const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const DAY_NAMES = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+const CURRENT_DATE = new Date();
 
-function createCalendarStructure () {
-    for(let k = 0; k < 30; k++) {
+function init() {
+    console.log(getDaysFromCurrentMonth());
+    console.log(getMonthDayStart());
+    createCalendarStructure();
+}
+/*** UTILS ***/
+var getDaysFromCurrentMonth = () => new Date(CURRENT_DATE.getFullYear(), CURRENT_DATE.getMonth() + 1, 0).getDate();
+var getMonthDayStart = () => {
+    let startDay = new Date(CURRENT_DATE.getFullYear(), CURRENT_DATE.getMonth(), 1).getDay();
+    return startDay === 0 ? 6 : (startDay - 1);
+}
+
+var createCalendarStructure = () => {
+    const days = getDaysFromCurrentMonth();
+    const startDay = getMonthDayStart();
+
+    for(let i = 0; i < (days + startDay); i++) {
         let dayContainer = document.createElement("div");
         dayContainer.className = "dayContainer";
-        for(let i = 0; i < 5; i++) {
-            let task = document.createElement("div");
-            let day = document.createElement("div");
-
-            task.className = i !== 4 ? "square" : "circle";
-            day.className = "day";
-            day.innerText = k < 9 ? `0${k + 1}` : k + 1; 
-            task.appendChild(day);
-            dayContainer.appendChild(task);
+        if(i < startDay) {
+            // Add class to empty start
+        } else {
+            let dayNumber = i - startDay;
+            for(let j = 0; j < 5; j++) {
+                let task = document.createElement("div");
+                let day = document.createElement("div");
+    
+                task.className = j !== 4 ? "square" : "circle";
+                day.className = "day";
+                day.innerText = dayNumber < 9 ? `0${dayNumber + 1}` : dayNumber + 1; 
+                task.appendChild(day);
+                dayContainer.appendChild(task);
+            }
         }
     
         CALENDAR.appendChild(dayContainer);
     }
-    // <div class="container">
-    //         <div class="task square first"></div>
-    //         <div class="task square second"></div>
-    //         <div class="task square third"></div>
-    //         <div class="task square fourth"></div>
-    //         <div class="task circle fifth">
-    //             <div class="day">1</div>
-    //         </div>
-    //     </div>
 }
 
-createCalendarStructure();
+init();
